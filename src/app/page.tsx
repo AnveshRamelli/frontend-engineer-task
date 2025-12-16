b4f9c6b10ee7c4c551f937bd3773e2b812c78b69
Anvesh Ramelli <anveshramelli1999@gmail.com>
2025-12-16 13:13:24 +0530

    added meta data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
 import DiscoveryMap from "@/components/discovery-map";
 import { PropertyListing } from "@/data/property-listing";
+import { Metadata } from "next";
 
 //TODO : Add meta data for this page
 // Page should serve via SSR
 // Do not add "use client" declarative
+
+// Added metadata for the page to improve SEO and provide a better user experience.
+export const metadata: Metadata = {
+  title: "Discover Properties on Map",
+  description: "Explore properties visually with interactive map discovery",
+};
 
 // TODO: Create a List view for these properties.
 // Use your own imagination while designing, please don't copy Propsoch's current UI.
@@ -12,9 +19,12 @@ import { PropertyListing } from "@/data/property-listing";
 // You can modify the Property Listing however you want. If you feel like creating an API and implementing pagination via that, totally your call.
 
 export default async function Page() {
+  // We can implement data fetching from DB or API here in future.
+  const data = PropertyListing;
+
   return (
     <div className="w-screen h-screen">
-      <DiscoveryMap allFilteredData={PropertyListing} />
+      <DiscoveryMap allFilteredData={data} />
     </div>
   );
 }
